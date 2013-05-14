f199559a702139fb418177f3024b260b2e23d2db
likai <youyuge@gmail.com>
2013-05-14 23:23:31 +0800

    Deleted old assets

--- assets/scripts/models/topic.js
+++
@@ -1,9 +0,0 @@
-define([
-    'backbone',
-], function(Backbone){
-    return Backbone.Model.extend({
-        urlRoot: function(){
-            return this.isNew() ? API_URL + '/topics' : API_URL + '/topic';
-        }
-    });
-});
